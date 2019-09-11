155892fc6f8844f46b4e8f79566e7d0a85424590
Kioh Han <kioh.han@gmail.com>
2019-09-10 23:08:11 -0400

    Notifications

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import MyButton from '../../util/MyButton';
 import PostScream from '../scream/PostScream';
+import Notifications from './Notifications';
 
 //MUI stuff
 import AppBar from '@material-ui/core/AppBar';
@@ -13,7 +14,7 @@ import Button from '@material-ui/core/Button';
 //Icons
 import AddIcon from '@material-ui/icons/Add';
 import HomeIcon from '@material-ui/icons/Home';
-import Notifications from '@material-ui/icons/Notifications';
+
 
 class Navbar extends Component {
     render() {
@@ -29,9 +30,7 @@ class Navbar extends Component {
                                     <HomeIcon />
                                 </MyButton>
                             </Link>
-                            <MyButton tip="Notifications">
-                                <Notifications />
-                            </MyButton>
+                            <Notifications />
                         </Fragment>
                     ):(         
                         <Fragment>
